374502c8f2c3d03b8b0a2baf0c1e519e39fb43d8
omardecai <earellano289@gmail.com>
2024-04-05 01:19:56 -0600

    modified 3-led-blink demo

--- demos/3-led-blink/README.md
+++ demos/3-led-blink/README.md
@@ -7,3 +7,8 @@ This program implements one state machine to toggle the LED once each second.
 * Blink the red and green LEDs in alternation.
 
 * Change the speed that the LEDs blink.  (e.g. twice or half as fast)
+
+## Modifications 
+
+* Modified the code to alternate between the red and green leds
+* Changed the speed from 250 hz per second to 120 hz per second
--- demos/3-led-blink/main.c
+++ demos/3-led-blink/main.c
@@ -1,29 +1,40 @@
-//Alternate LEDs from Off, Green, and Red
+// Alternate LEDs from Off, Green, and Red
 #include <msp430.h>
 #include "libTimer.h"
 #include "led.h"
 
-int main(void) {
+int main(void)
+{
   P1DIR |= LEDS;
   P1OUT &= ~LED_GREEN;
   P1OUT |= LED_RED;
 
-  configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
-  enableWDTInterrupts();	/* enable periodic interrupt */
-  
-  or_sr(0x18);			/* CPU off, GIE on */
+  configureClocks();     /* setup master oscillator, CPU & peripheral clocks */
+  enableWDTInterrupts(); /* enable periodic interrupt */
+
+  or_sr(0x18); /* CPU off, GIE on */
 }
 
 // global state var to count time
 int secondCount = 0;
 
-void
-__interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
+void __interrupt_vec(WDT_VECTOR) WDT() /* 250 interrupts/sec */
 {
-  secondCount ++;
-  if (secondCount >= 250) { 	/* once each sec... */
-    secondCount = 0;		/* reset count */
-    P1OUT ^= LED_GREEN;		/* toggle green LED */
-  }
-} 
+  secondCount++;
+  if (secondCount >= 120)
+  {                  /* once each sec... */
+    secondCount = 0; /* reset count */
 
+    if (P1OUT & LED_RED)
+    {
+      P1OUT &= ~LED_RED;  // Turn off red LED
+      P1OUT |= LED_GREEN; // Turn on green LED
+    }
+    else
+    {
+
+      P1OUT &= ~LED_GREEN; /* toggle green LED */
+      P1OUT |= LED_RED;
+    }
+  }
+}
